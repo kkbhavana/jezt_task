858cd8d988c987178178b0d02292ac1a66f1b949
kkbhavana <kkbhavana6@gmail.com>
2023-09-08 10:59:02 +0530

    initial

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,7 @@ from . import views
 from .views import TaskListCreateView, TaskUpdateView, TaskDeleteView, TaskDetailView
 
 urlpatterns=[
-    path('home',views.homepage,name='home'),
+    path('home/',views.homepage,name='home'),
     path('list-create/',TaskListCreateView.as_view(),name='list_create'),
     path('update/',TaskUpdateView.as_view(),name='list_create'),
     path('delete/',TaskDeleteView.as_view(),name='list_create'),
